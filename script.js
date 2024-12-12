91964a33680e886d9018f2c295069b6a2d8fe452
Jatin5784 <75659443+Jatin5784@users.noreply.github.com>
2024-12-12 12:28:36 +0530

    Added conditons for paper and scissor.     Refined the code.     Added final score conditons.

--- script.js
+++ script.js
@@ -1,48 +1,118 @@
-const randomValue=Math.random(); //Generate random value
-
-if (randomValue<0.33){
-
-    console.log("paper");
-}
-else if(randomValue<0.66){
-    console.log("rock");
-}
-else{
-    console.log("scissors");
-}
 
 
 let playerScore=0;          //Initial player score
 let computerScore=0;        //Initial computer score
 
-//Takes input and prints it.
-const input=prompt("Enter the number");
-console.log("you entered "+ input);
+for(let i =1; i<=5;i++){
 
+    const randomValue=Math.random(); //Generate random value
+
+if (randomValue<0.33){
+
+    console.log("Computer entered: paper");
+}
+else if(randomValue<0.66){
+    console.log("Computer entered: rock");
+}
+else{
+    console.log("Computer entered: scissors");
+}
+
+//Takes input and prints it.
+let input=prompt("Enter the Word -");
+console.log("You entered: "+ input);
+
+if(input==="rock"||input==="Rock"||input=="paper"||input=="Paper"||input=="scissors"||input=="Scissors"||input=="scissor"||input=="Scissor"){
 
 //----------Checking for Rock----------- 
-if(input=="rock"){
+    if(input=="rock"){
 
-if( randomValue<0.33){
-console.log("you lose");
+        if( randomValue<0.33){
+        console.log("you lose");
+    
+    
+    
+        computerScore=computerScore+1;
+        }
+    
+        else if(randomValue<0.66){
+        console.log("It's a tie");
+        }
+    
+        else{
+        console.log("You win!");
+        playerScore=playerScore+1;
+       
+        }
+    
+    }
 
-if(playerScore>0) { playerScore=playerScore-1}
 
-computerScore=computerScore+1;
+    //----------Checking for paper-------
+    if(input=="paper"||input=="Paper"){
+
+        if( randomValue<0.33){
+        console.log("It's a tie");
+        }
+    
+        else if(randomValue<0.66){
+        console.log("You win!");
+        playerScore=playerScore+1;
+        }
+    
+        else{
+        console.log("You lose!");
+        computerScore=computerScore+1;
+       
+        }
+    
+    }
+
+
+    //----------Checking for scissor-------
+
+    if(input=="scissor"||input=="Scissor"){
+
+        if( randomValue<0.33){
+        console.log("You win!");
+        playerScore=playerScore+1;
+
+        }
+    
+        else if(randomValue<0.66){
+        console.log("You lose!");
+        computerScore=computerScore+1;
+
+        }
+    
+        else{
+        console.log("It's a tie");
+       
+        }
+    }
 }
 
-else if(randomValue<0.66){
-    console.log("It's a tie");
-}
+
 
 else{
-    console.log("You win!");
-    playerScore=playerScore+1;
-    if(computerScore>0) { computerScore=computerScore-1}
+alert("Enter proper Word!"); 
+ 
+ i=i-1;
 }
 
-}
 
+
+}
 //Prints final score
 console.log("Your Score:"+playerScore);
 console.log("Computer Score: "+computerScore);
+if(playerScore>computerScore){
+    console.log("You won the Game!")
+}
+else if (playerScore===computerScore){
+    console.log("The game ended with a tie.")
+}
+else{
+    console.log("You lost the Game!")
+    
+}
